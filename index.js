bf0cd4e72e1a0927b512bf1f3aa68ff120e9e1b1
Justin Eastin <worldmisanthropy199@gmail.com>
2022-01-18 02:20:09 -0500

    Update index.js
    fixed duplicate link

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
-
 const express = require('express');
   morgan = require('morgan');
   bodyParser = require('body-parser');
